6b55110d077c938f83863d03496f02a4da0e3f17
Mattias <76684346+Drynxx@users.noreply.github.com>
2021-08-31 15:39:54 +0300

    source code

---
+++ whale_translate.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <conio.h>
+int main()
+{
+
+  //This program was made to translate any human speech into a whale language
+  //Whales pronunce every u‘s and e‘s twice, but we got it to work!
+  std::cout << "====================\n\n";
+  std::cout <<" Enter text : \n\n";
+  std::cout << "====================\n\n";
+  std::string input;
+  getline(std::cin,input);
+
+
+  std::vector<char> vowels;
+
+  vowels.push_back('a');
+  vowels.push_back('e');
+  vowels.push_back('i');
+  vowels.push_back('o');
+  vowels.push_back('u');
+
+  std::vector<char> whale_lang;
+
+  for (int i = 0; i < input.size(); i++)
+  {
+    for (int j = 0; j < vowels.size(); j++)
+    {
+
+      if (input[i] == vowels[j])
+      {
+        whale_lang.push_back(input[i]);
+        if (input[i] == 'e' || input [i] == 'u')
+        {
+        whale_lang.push_back(input[i]);
+        }
+      }
+    }
+  }
+
+  system("cls");
+  std::cout << "====================\n\n";
+  std::cout << "The program translates now into Whale Language....\n\n";
+  std::cout << "====================\n\n";
+
+  for (int i = 0; i < whale_lang.size(); i++)
+  {
+    std::cout << whale_lang[i];
+  }
+  std::cout << "\n";
+
+}
